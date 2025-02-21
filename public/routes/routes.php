790509ab93cc0bd8aee12e150cd7343cd6882e1d
Alice Cavalcanti <alicecavalcanti27@gmail.com>
2025-02-20 22:01:19 -0300

    wip: routes of project

--- public/routes/routes.php
+++ public/routes/routes.php
@@ -1,1 +1,12 @@
 <?php
+
+$router = [
+    'GET' => [
+        '/' => 'listClientsController.php'
+    ],
+    'POST' => [
+        '/saveClients' => 'saveClientsController.php',
+        '/editClients' => 'editClientsController.php',
+        '/deleteClients' => 'deleteClientsController.php',
+    ],
+];
